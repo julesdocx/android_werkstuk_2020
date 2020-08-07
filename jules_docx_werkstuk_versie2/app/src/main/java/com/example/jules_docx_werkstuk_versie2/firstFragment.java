4fc3b30621ef7248de54968bd53a5841bb7936ff
docxjules <jules.docx@gmail.com>
2020-08-07 13:14:43 +0200

    listView and Firebase Storage
    listview with api imageURL

--- jules_docx_werkstuk_versie2/app/src/main/java/com/example/jules_docx_werkstuk_versie2/firstFragment.java
+++ jules_docx_werkstuk_versie2/app/src/main/java/com/example/jules_docx_werkstuk_versie2/firstFragment.java
@@ -7,10 +7,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -32,19 +34,26 @@ public class firstFragment extends Fragment {
     ListView listView;
     String titles[] = {"Celebrity Rehab", "Dance of the Young", "Personel Horizon"};
     String artists[] = {"Katie Vick", "Otro", "Iku"};
-    String imgUrls[] = {};
-
+    String imgUrls[] = {"https://firebasestorage.googleapis.com/v0/b/jules-docx-androidstudio.appspot.com/o/102630150_558610248183627_8227748301609830273_n.jpg?alt=media&token=7f92cb7e-1fd0-4207-b37b-cc1f88695e98",
+                        "https://firebasestorage.googleapis.com/v0/b/jules-docx-androidstudio.appspot.com/o/106011458_263253914936711_756682807127883080_n.png?alt=media&token=5dd2483a-245f-4c5e-8e0f-97fea09fef0f",
+                        "https://firebasestorage.googleapis.com/v0/b/jules-docx-androidstudio.appspot.com/o/56764472_579673252530597_7096499730749849600_n.png?alt=media&token=587aef03-83fe-4ef1-ac51-7d3728768d5d"};
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view =  inflater.inflate(R.layout.fragment_first, container, false);
-        imageView = (ImageView) view.findViewById(R.id.testImage);
-        Picasso.get().load("https://i1.sndcdn.com/artworks-000557708352-yhl2hm-t500x500.jpg").into(imageView);
+        //imageView = (ImageView) view.findViewById(R.id.testImage);
         // Inflate the layout for this fragment
 
         listView = view.findViewById(R.id.artworkList);
+        MyAdapter adapter = new MyAdapter(this.getContext(), titles, artists, imgUrls);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
+            }
+        });
         return view;
 
     }
@@ -74,9 +83,11 @@ public class firstFragment extends Fragment {
             TextView Title = row.findViewById(R.id.textViewTitle);
             TextView Artist = row.findViewById(R.id.textViewArtist);
 
+
+            Picasso.get().load(imgUrl[position]).into(images);
             Title.setText(this.title[position]);
             Artist.setText(this.artist[position]);
-            
+
             return row;
         }
     }
